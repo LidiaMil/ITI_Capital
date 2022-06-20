0e47bb3d79946c75bcba27540ccb1912a7eacd34
LidiaMil <lidamilos@rambler.ru>
2022-06-20 19:27:47 +0300

    feat: refactor ws

--- src/trade/trade.module.ts
+++ src/trade/trade.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Trade } from './models/trade.model';
 import { TradeController } from './trade.controller';
+import { TradeGateway } from './trade.gateway';
 import { TradeService } from './trade.service';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Trade])],
+  imports: [SequelizeModule.forFeature([Trade]), TradeGateway],
   providers: [TradeService],
   controllers: [TradeController],
+  exports: [TradeService],
 })
 export class TradeModule {}
