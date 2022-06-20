102b37a08bfb7a2f4ec55ab4c725c52c95a18fd1
LidiaMil <lidamilos@rambler.ru>
2022-06-20 19:26:50 +0300

    feat: refactor еуые

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,18 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import mock from './mock';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    await mock()
   });
 
   it('/ (GET)', () => {
--- test/mock.ts
+++ test/mock.ts
@@ -3,24 +3,26 @@ import { Trade } from "../src/trade/models/trade.model";
 
 export default async function () {
     try {
-        await Security.bulkCreate([
-            {
-                seccode: Math.random().toString(36).substr(2, 5),
-                price: Math.random() * 10000 + 1,
-                isin: Math.random().toString(36).substr(2, 5),
-                lotsize: Math.random() * 10000 + 1,
-            }
-        ]);
-
-        await Trade.bulkCreate([
-            {
-                price: Math.random() * 10000 + 1,
-                size: Math.random() * 10000 + 1,
-                side: Math.random() * 10000 + 1,
-                security: Math.random() * 10000 + 1,
-                client: 'test' + Math.random() * 10000 + 1,
-            }
-        ]);
+        for (let i = 0; i < 10; i++) {
+            await Security.bulkCreate([
+                {
+                    seccode: Math.random().toString(36).substr(2, 5),
+                    price: Math.random() * 10000 + 1,
+                    isin: Math.random().toString(36).substr(2, 5) + 'isin',
+                    lotsize: Math.random() * 10000 + 1,
+                }
+            ]);
+    
+            await Trade.bulkCreate([
+                {
+                    price: Math.random() * 10000 + 1,
+                    size: Math.random() * 10000 + 1,
+                    side: Math.random() * 10000 + 1,
+                    security: Math.random() * 10000 + 1,
+                    client: 'test' + i,
+                }
+            ]);
+        }
     }
     catch (e) {
         console.log('[createTestDataModels]', e);
