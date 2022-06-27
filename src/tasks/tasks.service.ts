fb3f68546ccded42f447511d6cc6e51479fe00db
LidiaMil <lidamilos@rambler.ru>
2022-06-27 09:24:12 +0300

    feat: Procfile

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,13 +11,13 @@ export class TasksService {
       private readonly securityService: SecurityService
     ) {}
   
-  @Cron('45 * * * * *')
-  handleCron() {
-    this.securityService.remove()
-    this.logger.debug('Called when the second is 45');
-  }
+  // @Cron('45 * * * * *')
+  // handleCron() {
+  //   this.securityService.remove()
+  //   this.logger.debug('Called when the second is 45');
+  // }
 
-  @Interval(10000)
+  @Interval(10000*10000*10000)
   handleInterval() {
     this.tradeService.remove()
     this.logger.debug('Called every 10 seconds');
